36e64307326545823e7159d507ea279ce9fbb3a3
Cas van den Bogaard <cas.vanden.bogaard@sogeti.com>
2022-11-28 16:22:23 +0100

    Fixed setup.py file

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup
 
-from wc_predict import __version__
-
 setup(
     name='wc_predict',
-    version=__version__,
+    version='0.1',
 
     url='https://github.com/CasvandenBogaard/wc_prediction_hackathon',
     author='Cas van den Bogaard',
